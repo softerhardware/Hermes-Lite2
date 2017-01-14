f57949c6bf6f8054526e5b30851ac86d14c7b39b
U-MGC\shaynal <shaynal@ORW-1X4DYZ1.mgc.mentorg.com>
2017-01-13 20:51:56 -0800

    Refactoring to include all RTL

--- firmware/Ethernet/Tx_send.v
+++ firmware/Ethernet/Tx_send.v
@@ -5,7 +5,7 @@
 //
 //  HPSDR - High Performance Software Defined Radio
 //
-//  Metis code. 
+//  Metis code.
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ module Tx_send (
 	input IP_valid,
 	input [7:0] Hermes_serialno,
 	input IDHermesLite,
-	input  [8:0]AssignNR,
+	input  [7:0]AssignNR,
 
 	input [7:0] PHY_Tx_data,
 	input [10:0] PHY_Tx_rdused,
@@ -53,7 +53,7 @@ module Tx_send (
 );
 
 
-// HPSDR specific			
+// HPSDR specific
 parameter HPSDR_frame = 8'h01;  	// HPSDR Frame type
 parameter Type_1 = 8'hEF;	    	// Ethernet Frame type
 parameter Type_2 = 8'hFE;
@@ -70,7 +70,7 @@ localparam
 reg [31:0] sequence_number = 0;
 reg [31:0] spec_seq_number = 0;
 
-reg [2:0] state;             	
+reg [2:0] state;
 
 reg [10:0] byte_no;
 
@@ -91,7 +91,7 @@ assign emuID[7]  = "L";
 assign emuID[8]  = "T";
 assign emuID[9]  = AssignNR;
 
-always @ (posedge tx_clock)	
+always @ (posedge tx_clock)
 begin
 case(state)
 
@@ -104,17 +104,17 @@ START:
 		if (run == 1'b0) begin
 			sequence_number <= 32'd0;  		// reset sequence numbers when not running.
 			spec_seq_number <= 32'd0;
-		end 
+		end
 		if (discovery && IP_valid) begin		// only respond if we have a valid IP address
 			udp_tx_request <= 1'b1;
 			udp_tx_length <= 11'd60;
 			state <= DISCOVER1;
-		end 		
-		else if (PHY_Tx_rdused > 11'd1023  && !Tx_reset && run) begin	// wait until we have at least 1024 bytes in Tx fifo													
+		end
+		else if (PHY_Tx_rdused > 11'd1023  && !Tx_reset && run) begin	// wait until we have at least 1024 bytes in Tx fifo
 			udp_tx_request <= 1'b1;
 			udp_tx_length <= 11'd1032;
 			state <= UDP1;
-		end	
+		end
 		else if (have_sp_data && wide_spectrum) begin		// Spectrum fifo has data available
 			udp_tx_request <= 1'b1;
 			udp_tx_length <= 11'd1032;
@@ -122,7 +122,7 @@ START:
 		end
    end
 
-// start sending UDP/IP data   
+// start sending UDP/IP data
 UDP1:
 	begin
 		udp_tx_request <= 1'b1;
@@ -134,21 +134,21 @@ UDP1:
 
 UDP2:
 	begin
-		if (byte_no < 11'd1031) begin // Total-1 	
+		if (byte_no < 11'd1031) begin // Total-1
 			if (udp_tx_active) begin
 				case (byte_no)
-					 11'd0: tx_data <= Type_2;					
-					 11'd1: tx_data <= HPSDR_frame; 
+					 11'd0: tx_data <= Type_2;
+					 11'd1: tx_data <= HPSDR_frame;
 					 11'd2: tx_data <= 8'h06;
 					 11'd3: tx_data <= sequence_number[31:24];
 					 11'd4: tx_data <= sequence_number[23:16];
 					 11'd5: begin tx_data <= sequence_number[15:8]; Tx_fifo_rdreq <= 1'b1; end
-					 11'd6: begin tx_data <= sequence_number[7:0]; Tx_fifo_rdreq <= 1'b1; end  	
+					 11'd6: begin tx_data <= sequence_number[7:0]; Tx_fifo_rdreq <= 1'b1; end
 					 11'd1029: Tx_fifo_rdreq <= 1'b0; // Total-3
-					 default: tx_data <= PHY_Tx_data;		 
-				endcase				
+					 default: tx_data <= PHY_Tx_data;
+				endcase
 				byte_no <= byte_no + 11'd1;
-			end 
+			end
 		end
 		else begin
 			sequence_number <= sequence_number + 1'b1;
@@ -156,7 +156,7 @@ UDP2:
 		end
 	end
 
-// start sending UDP/IP data   
+// start sending UDP/IP data
 WIDE1:
 	begin
 		udp_tx_request <= 1'b1;
@@ -168,24 +168,24 @@ WIDE1:
 
 WIDE2:
 	begin
-		if (byte_no < 11'd1031) begin // Total-1 	
+		if (byte_no < 11'd1031) begin // Total-1
 			if (udp_tx_active) begin
 				case (byte_no)
-					 11'd0: tx_data <= Type_2;					
-					 11'd1: tx_data <= HPSDR_frame; 
+					 11'd0: tx_data <= Type_2;
+					 11'd1: tx_data <= HPSDR_frame;
 					 11'd2: tx_data <= 8'h04;
 					 11'd3: tx_data <= spec_seq_number[31:24];
 					 11'd4: tx_data <= spec_seq_number[23:16];
 					 11'd5: begin tx_data <= spec_seq_number[15:8]; sp_fifo_rdreq <= 1'b1; end
-					 11'd6: begin tx_data <= spec_seq_number[7:0]; sp_fifo_rdreq <= 1'b1; end  	
+					 11'd6: begin tx_data <= spec_seq_number[7:0]; sp_fifo_rdreq <= 1'b1; end
 					 11'd1029: sp_fifo_rdreq <= 1'b0; // Total-3
-					 default: tx_data <= sp_fifo_rddata;		 
-				endcase				
+					 default: tx_data <= sp_fifo_rddata;
+				endcase
 				byte_no <= byte_no + 11'd1;
-			end 
+			end
 		end
 		else begin
-			spec_seq_number <= spec_seq_number + 1'b1; 
+			spec_seq_number <= spec_seq_number + 1'b1;
 			state <= START;
 		end
 	end
@@ -204,7 +204,7 @@ DISCOVER2:
 		if (byte_no < 11'd59) begin // Total-1
 			if (udp_tx_active) begin
 				case (byte_no)
-					 11'd0: tx_data <= Type_2;					
+					 11'd0: tx_data <= Type_2;
 					 11'd1: tx_data <= run ? 8'h03 : 8'h02;
 					 11'd2: tx_data <= This_MAC[47:40];
 					 11'd3: tx_data <= This_MAC[39:32];
@@ -224,7 +224,7 @@ DISCOVER2:
 					 11'd17: tx_data <= emuID[8]; // "T"
 					 11'd18: tx_data <= emuID[9]; // NR
 					 default: tx_data <= emuID[0];
-				endcase				
+				endcase
 				byte_no <= byte_no + 11'd1;
 			end
 		end
