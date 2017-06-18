58a84ca209a15af2bd3fd24085ebf7beb7c0a3e5
ji1udd <takashi@ba2.so-net.ne.jp>
2017-06-18 10:40:25 +0900

    Fixed return code

--- compact-trx/powercontrol/software/RaspberryPi/shutdown.py
+++ compact-trx/powercontrol/software/RaspberryPi/shutdown.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
-# coding:utf-8
+# coding:utf-8 
 import time
 import RPi.GPIO as GPIO
 import os
-
+ 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-
+ 
 while True:
     GPIO.wait_for_edge(14, GPIO.RISING)
     sw_counter = 0
-
+ 
     while True:
         sw_status = GPIO.input(14)
         if sw_status == 1:
@@ -20,5 +20,5 @@ while True:
                 break
         else:
             break
-
+ 
         time.sleep(0.01)
