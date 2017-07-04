1f18ff8a1bbfabb9e09f408ce7df694aba5054a6
U-MGC\shaynal <shaynal@ORW-1X4DYZ1.mgc.mentorg.com>
2017-07-03 20:05:19 -0700

    Parts DB contains more information for PCB assembly houses

--- hardware/hl/bom/mkstandardbom.py
+++ hardware/hl/bom/mkstandardbom.py
@@ -6,6 +6,8 @@ optionset = set(['NOSWCLK','REGPA','PA','LVDS25','ETH25','ETH','VERSA','VBIAS','
 	'RXLPF','RXNPOL','TXLPF','TXPREAMP','TXSW','TXLP','LED','FRONTIO','ADC','THERMAL',
 	'PATR','EXTPTT','EXTFILTER','VERSAOSC','CASE','PROGRAMMER'])
 
+## 'NOASSEMBLY' to include parts needed by not put on by assembly house
+
 bom = BOM.BOM("../hermeslite.xml",optionset=optionset)
 
 pre = """\\section*{Hermes-Lite 2.0beta3 BOM}
