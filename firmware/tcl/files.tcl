575391db7a9c3153567fefad006996de5a2f45be
U-MGC\shaynal <shaynal@ORW-1X4DYZ1.mgc.mentorg.com>
2018-06-02 21:13:54 -0700

    Automated Makefile-based build

---
+++ firmware/tcl/files.tcl
@@ -0,0 +1,59 @@
+
+
+set_global_assignment -name VERILOG_FILE rtl/hermeslite.v
+set_global_assignment -name VERILOG_FILE rtl/fifos.v
+set_global_assignment -name VERILOG_FILE rtl/ad9866.v
+set_global_assignment -name VERILOG_FILE rtl/ad9866pll.v
+set_global_assignment -name VERILOG_FILE rtl/ad9866ctrl.v
+set_global_assignment -name VERILOG_FILE rtl/ethpll.v
+
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/radio.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/varcic.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/cic.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/cic_comb.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/cic_integrator.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/cordic.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/cpl_cordic.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/receiver.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/firfilt.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/CicInterpM5.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/firram36I_1024.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/FirInterp8_1024.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/firram36.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/vna_scanner.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/firrom/firromH.v
+set_global_assignment -name VERILOG_FILE rtl/radio_openhpsdr1/firrom/firromI_1024.v
+
+set_global_assignment -name VERILOG_FILE rtl/ethernet/udp_send.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/udp_recv.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/rgmii_send.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/rgmii_recv.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/phy_cfg.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/network.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/mdio.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/mac_send.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/mac_recv.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/ip_send.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/ip_recv.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/icmp.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/dhcp.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/crc32.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/arp.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/ddio_out.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/ddio_in.v
+set_global_assignment -name VERILOG_FILE rtl/ethernet/icmp_fifo.v
+
+set_global_assignment -name VERILOG_FILE rtl/dsopenhpsdr1.v
+set_global_assignment -name VERILOG_FILE rtl/usopenhpsdr1.v
+
+set_global_assignment -name VERILOG_FILE rtl/cdc_sync.v
+set_global_assignment -name VERILOG_FILE rtl/sync.v
+
+set_global_assignment -name VERILOG_FILE rtl/control.v
+set_global_assignment -name VERILOG_FILE rtl/debounce.v
+set_global_assignment -name VERILOG_FILE rtl/i2c_master.v
+set_global_assignment -name VERILOG_FILE rtl/i2c_init.v
+set_global_assignment -name VERILOG_FILE rtl/i2c_bus2.v
+set_global_assignment -name VERILOG_FILE rtl/i2c.v
+set_global_assignment -name VERILOG_FILE rtl/slow_adc.v
+
