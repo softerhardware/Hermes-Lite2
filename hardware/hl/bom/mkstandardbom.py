83bd3011afe69bff0be21747d263219074374d9c
SofterHardware <steve@softerhardware.com>
2018-06-19 21:16:32 -0700

    Updated BOM generation scripts

--- hardware/hl/bom/mkstandardbom.py
+++ hardware/hl/bom/mkstandardbom.py
@@ -2,13 +2,12 @@
 
 import BOM
 
-optionset = set([])
-
-## 'NOASSEMBLY' to include parts needed but not put on by assembly house
+## Add ADNI to include assembly DNI parts that are required
+optionset = set(["ADNI"])
 
 bom = BOM.BOM("../hermeslite.xml",optionset=optionset)
 
-pre = """\\section*{Hermes-Lite 2.0build6 BOM}
+pre = """\\section*{Hermes-Lite 2.0build7 BOM}
 Standard Build - \\today"""
 
 bom.LaTeXPrint(pre,['Mouser','Digi-Key'])
