abcd90b6dea2b3288624630d50306357162ef82a
osboxes.org <steve@softerhardware.com>
2019-02-11 22:56:00 -0800

    Build8 updates

--- hardware/hl/bom/mkstandardbom.py
+++ hardware/hl/bom/mkstandardbom.py
@@ -7,7 +7,7 @@ optionset = set(["ADNI"])
 
 bom = BOM.BOM("../hermeslite.xml",optionset=optionset)
 
-pre = """\\section*{Hermes-Lite 2.0build7 BOM}
+pre = """\\section*{Hermes-Lite 2.0build8 BOM}
 Standard Build - \\today"""
 
 bom.LaTeXPrint(pre,['Mouser','Digi-Key'])
