ead712b6e16be6f5f151e8852aebe7013a18698a
osboxes.org <steve@softerhardware.com>
2019-09-01 13:06:27 -0700

    Added 40mm enclosure

---
+++ hardware/enclosure/endcaps/kf7o/hl2_40b/gerber/rename.py
@@ -0,0 +1,14 @@
+
+
+import os
+
+
+li = ["GBL","GBO","GBS","GML","GTL","GTO","GTS","TXT"]
+
+
+oldroot = "endcap"
+newroot = "hl2_40b"
+
+for f in li:
+	os.rename("{0}/{1}.{2}".format(newroot,oldroot,f),"{0}/{0}.{1}".format(newroot,f))
+
